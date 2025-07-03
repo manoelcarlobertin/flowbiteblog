b65c07eb358d6d34bf41d71d26a7219ba1473e9f
BertinManoelBr <manoelcarlobertin@outlook.com>
2025-07-03 02:57:40 -0300

    só admin poderá fazer determinadas ações.

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,5 +1,6 @@
 class BlogPostsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  before_action :authenticate_admin!, except: %i[ index show ]
   before_action :set_blog_post, only: %i[ show edit update destroy ]
 
   def index
